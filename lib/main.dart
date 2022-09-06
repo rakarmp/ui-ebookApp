61e57f95cc0374af8ce7b3738a7ba83831e16453
AtyoungR <sejekim888@gmail.com>
2022-09-06 16:41:59 +0700

    Rebuild Tampilan Masih Dalam Progres

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Cuy Perpus',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
       home: HomeScreen(),
     );
