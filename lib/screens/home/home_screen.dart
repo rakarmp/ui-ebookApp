3741980ea993dfc6f0e5d78e9e8feaf5029c03c5
AtyoungR <sejekim888@gmail.com>
2022-09-07 11:12:05 +0700

    Menambahkan Tampilan Home

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,69 @@
+import 'package:elib/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HomeScreen());
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+@override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: NavigationExample());
+  }
+}
+
+class NavigationExample extends StatefulWidget {
+  const NavigationExample({super.key});
+
+  @override
+  State<NavigationExample> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<NavigationExample> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.home_max),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.article_rounded),
+            label: 'Books',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.account_circle),
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          
+        ),
+        Container(
+          child: HomeApp(),
+        ),
+        Container(
+          color: Colors.blue,
+          alignment: Alignment.center,
+          child: const Text('Page 3'),
+        ),
+      ][currentPageIndex],
+    );
+  }
+}
