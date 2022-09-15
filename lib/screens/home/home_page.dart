a521d29ba85a270c2471c02458364ac95d28fccf
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-09-15 10:49:29 +0700

    Fix Position Dan Delete Icon

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -29,10 +29,6 @@ class _HomePageState extends State<HomePage> {
         margin: EdgeInsets.symmetric(horizontal: 30),
         child: Row(
           children: [
-            Container(
-              width: 50,
-              height: 50,
-            ),
             SizedBox(width: 8),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -47,11 +43,11 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ),
-            Spacer(),
-            Image.asset(
-              'assets/icons/icon-menu.png',
-              width: 18,
-            ),
+            // Spacer(),
+            // Image.asset(
+            //   'assets/icons/icon-menu.png',
+            //   width: 18,
+            // ),
           ],
         ),
       );
@@ -60,7 +56,7 @@ class _HomePageState extends State<HomePage> {
     Widget searchField() {
       return Container(
         margin: EdgeInsets.symmetric(horizontal: 30),
-        child: TextField(
+        child: TextFormField(
           decoration: InputDecoration(
             hintText: 'Cari Buku Favorite Kamu',
             hintStyle: mediumText12.copyWith(color: greyColor),
