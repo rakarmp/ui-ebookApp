8c2175ddfb3d78b8c7e3fcc20a7d349a48221cdf
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-09-27 08:16:40 +0700

    Final Pull To Refresh

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -228,96 +228,121 @@ class _HomePageState extends State<HomePage> {
       // );
     }
 
+    final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+        new GlobalKey<RefreshIndicatorState>();
+
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: ListView(
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 30),
-            decoration: BoxDecoration(
-              color: whiteColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30),
+      body: RefreshIndicator(
+        key: _refreshIndicatorKey,
+        color: Colors.lightBlue,
+        backgroundColor: Colors.white,
+        strokeWidth: 3.0,
+        onRefresh: () async {
+          return Future.delayed(Duration(seconds: 2), (() {
+            setState(() {
+              _getData();
+            });
+          }));
+        },
+        child: ListView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 30),
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  header(),
+                  SizedBox(height: 30),
+                  searchField(),
+                  SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Text(
+                      'Recent Book',
+                      style: semiBoldText16.copyWith(color: blackColor),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  recentBook(),
+                ],
               ),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                header(),
-                SizedBox(height: 30),
-                searchField(),
-                SizedBox(height: 30),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Text(
-                    'Recent Book',
-                    style: semiBoldText16.copyWith(color: blackColor),
-                  ),
-                ),
-                SizedBox(height: 12),
-                recentBook(),
-              ],
+            // listCategories(),
+            Container(
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.height * 0.1,
+              child: categories == null || categories!.isEmpty
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: categories!.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () {
+                            setState(() {
+                              _isSelected = index;
+                            });
+                          },
+                          child: Expanded(
+                              child: Container(
+                            margin: EdgeInsets.only(top: 30, right: 12),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 10),
+                            decoration: BoxDecoration(
+                              color: _isSelected == index
+                                  ? blueColor
+                                  : transParentColor,
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: Text(
+                              categories![index].namaKategori.toString(),
+                              style: semiBoldText14.copyWith(
+                                  color: _isSelected == index
+                                      ? whiteColor
+                                      : greyColor),
+                            ),
+                          )),
+                        );
+                      },
+                    ),
             ),
-          ),
-          // listCategories(),
-          Container(
-            width: MediaQuery.of(context).size.width * 1,
-            height: MediaQuery.of(context).size.height * 0.1,
-            child: categories == null || categories!.isEmpty
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: categories!.length,
-                    itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {
-                          setState(() {
-                            _isSelected = index;
-                          });
-                        },
-                        child: Expanded(
-                            child: Container(
-                          margin: EdgeInsets.only(top: 30, right: 12),
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-                          decoration: BoxDecoration(
-                            color: _isSelected == index
-                                ? blueColor
-                                : transParentColor,
-                            borderRadius: BorderRadius.circular(6),
-                          ),
-                          child: Text(
-                            categories![index].namaKategori.toString(),
-                            style: semiBoldText14.copyWith(
-                                color: _isSelected == index
-                                    ? whiteColor
-                                    : greyColor),
-                          ),
-                        )),
-                      );
-                    },
-                  ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 30, top: 30),
-            child: Text(
-              'Trending Now',
-              style: semiBoldText16.copyWith(color: blackColor),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, top: 30),
+              child: Text(
+                'Trending Now',
+                style: semiBoldText16.copyWith(color: blackColor),
+              ),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.1,
-            height: MediaQuery.of(context).size.height * 1,
-            child: categories == null || categories!.isEmpty
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                :
-          trendingBook(), ),
-          SizedBox(height: 30),
-        ],
+            Container(
+              width: MediaQuery.of(context).size.width * 0.1,
+              height: MediaQuery.of(context).size.height * 1,
+              child: categories == null || categories!.isEmpty
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : trendingBook(),
+            ),
+            SizedBox(height: 30),
+          ],
+        ),
+      ),
+      // Button Sample Refresh Bisa DiBlok Comment
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          _refreshIndicatorKey.currentState?.show();
+        },
+        // icon: const Icon(Icons.refresh),
+        label: const Icon(Icons.refresh),
       ),
     );
   }
